3890294f80de578b40759767c526ebfdb8754337
JuJin1324 <jujin1324@daum.net>
2023-08-27 15:12:27 +0900

    Initial commit

---
+++ src/main/java/starter/event/driven/EventDrivenStarterApplication.java
@@ -0,0 +1,13 @@
+package starter.event.driven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventDrivenStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/starter/event/driven/EventDrivenStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package starter.event.driven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
